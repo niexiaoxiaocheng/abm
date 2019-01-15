de79ecaebd0151d5169f913ec22473c6a2c4710f
Hubert.Hu <huberthu@augmentum.com.cn>
2019-01-15 17:46:54 +0800

    add config

--- abm/src/main.js
+++ abm/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import fly from './utils/fly'
+import 'mpvue-weui/src/style/weui.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$fly = fly
 App.mpType = 'app'
 
 const app = new Vue(App)
